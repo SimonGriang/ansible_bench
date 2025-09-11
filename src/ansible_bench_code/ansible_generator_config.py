457123213d37ca0ba066f55ef1941bbdf6ebc2a2
Simon Göttsberger <125456063+SimonGriang@users.noreply.github.com>
2025-09-11 13:21:25 +0200

    Update LLAMAFILE_VERSION and file paths

--- src/ansible_bench_code/ansible_generator_config.py
+++ src/ansible_bench_code/ansible_generator_config.py
@@ -3,13 +3,13 @@
 from pathlib import Path
 
 
-LLAMAFILE_VERSION = "0.6.2"
+LLAMAFILE_VERSION = "0.8.17"
 
 TOKENIZER_MODELS_PATH = Path("/home/studgoetsi5301/documents/tokenizer")
 """Path to the base directory of the transformers and pytorch model files."""
 LLAMAFILE_PATH = Path("/home/studgoetsi5301/documents/llamafile/llamafile-0.8.17")
 """Path to the llamafile executable."""
-GGUF_PATH = Path("E:/") / "models" / "gguf"
+GGUF_PATH = Path("/home/studgoetsi5301/documents/llamafile")
 """Path to the directory with the GGUF files of the models."""
-LLAMAFILE_OUTPUT_LOG = Path("C:/") / "home" / "ma_simon" / "llamafile_output"
+LLAMAFILE_OUTPUT_LOG = Path("/home/studgoetsi5301/documents/ansible_bench/output")
 """Path to the directory where the llamafile output log files are stored."""
