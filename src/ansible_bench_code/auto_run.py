b55e829a041c64a5e1522750aee11e25df1bb61e
SimonGriang <s.goettsberger@live.de>
2025-10-13 06:59:03 +0200

    run granite to codestral, to gptoss, to phi and to llama

--- src/ansible_bench_code/auto_run.py
+++ src/ansible_bench_code/auto_run.py
@@ -7,15 +7,15 @@ import time
 
 # List of commands to run
 commands = [
-    "python ansible_generator.py -m gpt-oss:20b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_qwen2.5:14b_english_exact/benchmark100 > ../../output/ollama_gpt-oss:20b_english_exact/benchmark100/qwen_to_gpt-oss_benchmark.log 2>&1",
-    #"python ansible_generator.py -m qwen2.5:14b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_english_exact/benchmark100 > ../../output/ollama_qwen2.5:14b_english_exact/benchmark100/deepseek_to_qwen_benchmark.log 2>&1",
-    "python ansible_generator.py -m deepseek-r1:14b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_english_exact/benchmark100 > ../../output/ollama_deepseek-r1:14b_english_exact/benchmark100/deepseek_to_deepseek_benchmark.log 2>&1",
-    "python ansible_generator.py -m gemma3:27b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_english_exact/benchmark100 > ../../output/ollama_gemma3:27b_english_exact/benchmark100/deepseek_to_gemma_benchmark.log 2>&1",
-    "python ansible_generator.py -m granite-code:20b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_english_exact/benchmark100 > ../../output/ollama_granite-code:20b_english_exact/benchmark100/deepseek_to_granite_benchmark.log 2>&1",
-    "python ansible_generator.py -m llama3.1:8b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_english_exact/benchmark100 > ../../output/ollama_llama3.1:8b_english_exact/benchmark100/deepseek_to_llama_benchmark.log 2>&1",
-    "python ansible_generator.py -m phi4:14b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_english_exact/benchmark100 > ../../output/ollama_phi4:14b_english_exact/benchmark100/deepseek_to_phi_benchmark.log 2>&1",
-    "python ansible_generator.py -m codestral:22b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_english_exact/benchmark100 > ../../output/ollama_codestral:22b_english_exact/benchmark100/deepseek_to_codestral_benchmark.log 2>&1",
-    "python ansible_generator.py -m gpt-oss:20b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_english_exact/benchmark100 > ../../output/ollama_gpt-oss:20b_english_exact/benchmark100/deepseek_to_gpt-oss_benchmark.log 2>&1",
+    #"python ansible_generator.py -m gpt-oss:20b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_gemma3:27b_english_exact/benchmark100 > ../../output/ollama_gpt-oss:20b_english_exact/benchmark100/gemma_to_gpt-oss_benchmark.log 2>&1",
+    "python ansible_generator.py -m qwen2.5:14b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_granite-code:20b_english_exact/benchmark100 > ../../output/ollama_qwen2.5:14b_english_exact/benchmark100/granite_to_qwen_benchmark.log 2>&1",
+    "python ansible_generator.py -m deepseek-r1:14b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_granite-code:20b_english_exact/benchmark100 > ../../output/ollama_deepseek-r1:14b_english_exact/benchmark100/granite_to_deepseek_benchmark.log 2>&1",
+    "python ansible_generator.py -m gemma3:27b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_granite-code:20b_english_exact/benchmark100 > ../../output/ollama_gemma3:27b_english_exact/benchmark100/granite_to_gemma_benchmark.log 2>&1",
+    "python ansible_generator.py -m granite-code:20b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_granite-code:20b_english_exact/benchmark100 > ../../output/ollama_granite-code:20b_english_exact/benchmark100/granite_to_granite_benchmark.log 2>&1",
+    "python ansible_generator.py -m llama3.1:8b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_granite-code:20b_english_exact/benchmark100 > ../../output/ollama_llama3.1:8b_english_exact/benchmark100/granite_to_llama_benchmark.log 2>&1",
+    "python ansible_generator.py -m phi4:14b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_granite-code:20b_english_exact/benchmark100 > ../../output/ollama_phi4:14b_english_exact/benchmark100/granite_to_phi_benchmark.log 2>&1",
+    "python ansible_generator.py -m codestral:22b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_granite-code:20b_english_exact/benchmark100 > ../../output/ollama_codestral:22b_english_exact/benchmark100/granite_to_codestral_benchmark.log 2>&1",
+    "python ansible_generator.py -m gpt-oss:20b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_granite-code:20b_english_exact/benchmark100 > ../../output/ollama_gpt-oss:20b_english_exact/benchmark100/granite_to_gpt-oss_benchmark.log 2>&1",
 ]
 
 for cmd in commands:
@@ -42,3 +42,8 @@ for cmd in commands:
         print(f">> Fertig: {cmd}")
     else:
         print(f">> FEHLER ({return_code}): {cmd}")
+
+
+
+########## useful prompts: ##########
+#     "python ansible_generator.py -m gemma3:27b -e ollama benchmark -d benchmark100 -tt exact -p prompts/ollama_deepseek-r1:14b_to_gemma3:27b_benchmark100_missing_files_prompts > ../../output/ollama_gemma3:27b_english_exact/benchmark100/deepseek_to_gemma_benchmark.log 2>&1", #to be deleted
