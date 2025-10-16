8331105a872c0bfa152d6accfa38d66e247f261a
SimonGriang <s.goettsberger@live.de>
2025-10-16 05:51:40 +0200

    codestral to gemma part 2, codestral to llama, codestral to granite

--- output/ollama_deepseek-r1:14b_english_exact/benchmark100/prompts_codestral:22b/molecule_test/ansible-role-bootstrap-JonasPammer/.devcontainer/postCreateCommand.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# avoid "detected dubious ownership in repository":
-git config --global --add safe.directory /workspaces/*
-
-python3 -m pip install -r requirements-dev.txt
-
-pre-commit install --install-hooks
-#pre-commit install --hook-type commit-msg
